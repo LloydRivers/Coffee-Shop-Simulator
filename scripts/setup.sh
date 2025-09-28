cf8d838efb744685c15b337e28aa0c3627941cb3
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-09-28 13:11:54 +0100

    folder setup script

---
+++ scripts/setup.sh
@@ -0,0 +1,2 @@
+mkdir -p api/{routes,models,services,config} database/{migrations,seeds} monitoring/{prometheus,grafana,alertmanager} scripts
+touch docker-compose.yml api/server.js scripts/setup.sh
