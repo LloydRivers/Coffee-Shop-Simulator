47cec648fa66141417ae9659da2ef216e860c9a9
Lloyd Rivers <86473036+LloydRivers@users.noreply.github.com>
2025-09-28 20:13:30 +0100

    switch to morgan logger

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { connectRedis } = require("./config/redisClient");
 const { connectPostGres } = require("./config/postgresClient");
+const logger = require("./config/logger");
+const cors = require("cors");
 
 const app = express();
 const port = 3333;
@@ -10,19 +12,19 @@ app.use(cors());
 
 (async () => {
   try {
-    console.log("Connecting to Postgres...");
+    logger.info("Connecting to Postgres...");
     await connectPostGres();
-    console.log("Postgres connected successfully");
+    logger.info("Postgres connected successfully");
 
-    console.log("Connecting to Redis...");
+    logger.info("Connecting to Redis...");
     await connectRedis();
-    console.log("Redis connected successfully");
+    logger.info("Redis connected successfully");
 
     app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`);
+      logger.info(`Example app listening on port ${port}`);
     });
   } catch (err) {
-    console.error("Startup failed", err);
+    logger.error("Startup failed", err);
     process.exit(1);
   }
 })();
